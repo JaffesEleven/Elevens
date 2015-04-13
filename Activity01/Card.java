e303d1da8f3708f729855635d76968de7284f4d1
brandonkennedy <brandon.kennedy@mrjaffesclass.com>
2015-04-13 12:00:27 -0700

    Added unit tests

--- Activity01/Card.java
+++ Activity01/Card.java
@@ -34,7 +34,9 @@ public class Card {
      *                  containing the point value of the card
      */
     public Card(String cardRank, String cardSuit, int cardPointValue) {
-
+        rank = cardRank;
+        suit = cardSuit;
+        pointValue = cardPointValue;
     }
 
 
@@ -43,7 +45,7 @@ public class Card {
      * @return this <code>Card's</code> suit.
      */
     public String suit() {
-
+        return suit;
     }
 
     /**
@@ -51,7 +53,7 @@ public class Card {
      * @return this <code>Card's</code> rank.
      */
     public String rank() {
-
+        return rank;
     }
 
    /**
@@ -59,7 +61,7 @@ public class Card {
      * @return this <code>Card's</code> point value.
      */
     public int pointValue() {
-
+        return pointValue;
     }
 
     /** Compare this card with the argument.
@@ -69,7 +71,14 @@ public class Card {
      *         false otherwise.
      */
     public boolean matches(Card otherCard) {
-
+        if(suit == otherCard.suit() && rank == otherCard.rank() && pointValue == otherCard.pointValue())
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 
     /**
@@ -84,6 +93,7 @@ public class Card {
      */
     @Override
     public String toString() {
+        return rank() + " of " + suit() + " (point value = " + pointValue() + ")";
 
     }
 }
---
+++ Activity01/CardTester.java
@@ -0,0 +1,18 @@
+package Activity01;
+
+/**
+ * This is a class that tests the Card class.
+ */
+public class CardTester {
+    /**
+    * The main method in this class checks the Card operations for consistency.
+    *  @param args is not used.
+    */
+    public static void main(String[] args) 
+    {
+        /* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+        Card card1 = new Card("Queen","Hearts", 12);
+        Card card2 = new Card("King", "Diamonds", 13);
+        Card card3 = new Card("Jack", "Spades", 11);
+    }
+}
